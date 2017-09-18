a1dab8d4189564a40c18eff79e2055106c762dea
Michael Hampton <24950358+Rauxz@users.noreply.github.com>
2017-09-17 21:25:51 -0700

    Added FPP modes

--- pypubg/constants.py
+++ pypubg/constants.py
@@ -1,5 +1,5 @@
 GAME_MODE_WILDCARD = 'all'
-GAME_MODES = ['solo', 'duo', 'squad', GAME_MODE_WILDCARD]
+GAME_MODES = ['solo', 'duo', 'squad', 'solo-fpp', 'duo-fpp', 'squad-fpp', GAME_MODE_WILDCARD]
 
 GAME_REGION_WILDCARD = 'all'
 GAME_REGIONS = ['as', 'na', 'agg', 'sea', 'eu', 'oc', 'sa', GAME_REGION_WILDCARD]
